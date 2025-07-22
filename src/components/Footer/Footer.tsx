7c3f60dcc37e56127f21163e29f41a9fe5642b8b
david <davidLiao@hahow.in>
2025-07-22 17:14:15 +0800

    fix: no need to fix footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 left-0 right-0 p-4 text-center text-sm transition-colors text-gray-500 hover:text-emerald-500">
+    <div className="mx-auto w-fit p-4 text-center text-sm transition-colors text-gray-500 hover:text-emerald-500">
       <a href="https://www.shanpig.com" target="_blank" rel="noopener noreferrer">
         @ 2025&nbsp; Shanpig Liao
       </a>
